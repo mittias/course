19d513ddb9a471f598ac3a18d6311c440b8ab9a1
mittias <mityaichick@gmail.com>
2019-01-17 12:28:41 +0300

    все ровно

--- src/js/script.js
+++ src/js/script.js
@@ -63,17 +63,18 @@ $('.main-nav__toggle').on('click', function(e) {
   $('.main-nav__list').toggleClass("main-nav__listOpen");
   e.preventDefault();
 });
+
 jQuery(document).ready(function ($) {
 
   let slideCount = $('#slider ul li').length;
   let slideWidth = $('#slider ul li').width();
   let slideHeight = $('#slider ul li').height();
   let sliderUlWidth = slideCount * slideWidth;
-  
+
   $('#slider').css({ width: slideWidth, height: slideHeight });
-  
+
   $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-  
+
     $('#slider ul li:last-child').prependTo('#slider ul');
 
     function moveLeft() {
@@ -103,4 +104,4 @@ jQuery(document).ready(function ($) {
         moveRight();
     });
 
-});   
+});
